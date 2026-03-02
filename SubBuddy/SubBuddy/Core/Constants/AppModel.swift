6e57cdcba0ccb1abbfc6260ae9cb3fbd4d519817
Togrul Niyazli <togrul.niyazli@mail.ru>
2026-03-02 20:33:31 +0400

    created Constants

---
+++ SubBuddy/SubBuddy/Core/Constants/AppModel.swift
@@ -0,0 +1,18 @@
+//
+//  AppModel.swift
+//  SubBuddy
+//
+//  Created by Toğrul Niyazlı on 28.02.26.
+//
+
+import Foundation
+
+struct AppModel {
+
+    let id: UUID
+    let name: String
+    let iconName: String
+    let price: Double
+    let rating: Double
+    let category: AppCategory
+}
